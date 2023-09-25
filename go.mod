9b952e9e98f204e066a4cb29cac9d5814d906f25
Tom Diacono <tomdiacono@wirelesslogic.com>
2023-09-25 18:54:26 +0100

    Init Commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module go-test
+
+go 1.21.1
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/invopop/jsonschema v0.9.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/sashabaranov/go-openai v1.15.3 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ playaround.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/invopop/jsonschema"
+	"github.com/sashabaranov/go-openai"
+)
+
+// Struct - Defined by user
+type Address struct {
+	Street string
+	City   string
+	State  string
+}
+
+type ReturnData struct {
+	Name    string
+	Age     int
+	Address Address
+}
+
+func main() {
+	var userType ReturnData
+	decodeData(`- Name: John Smith - Age: 30 - Address: 123 Main St, New York, NY`, &userType)
+
+}
+
+func decodeData(inputData string, dataStructure any) {
+	// Create JSON schema from struct
+	jsonFormat := jsonschema.Reflect(dataStructure)
+	// Get JSON Schema in string format
+	jsonFormatInString, err := jsonFormat.MarshalJSON()
+
+	client := openai.NewClient("{FILL ME IN WITH .ENV}")
+	response, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role: openai.ChatMessageRoleSystem,
+					Content: `
+					You are a parser unstructured text data. I need you to return raw JSON in your answers.
+					This JSON will need to be in the format described in the message- and need to
+					`,
+				},
+				{
+					Role: openai.ChatMessageRoleUser,
+					Content: fmt.Sprintf(`
+						%s
+						I'd like to format the following data in the above JSON string. It has to be returned like this as it's going to be parsed by code.
+						The data is:
+                        %s
+				`, jsonFormatInString, inputData),
+				},
+			},
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	// Get the first message choice
+	completion := response.Choices[0].Message.Content
+
+	// Convert AI generated JSON to out struct
+	err = json.Unmarshal([]byte(completion), &dataStructure)
+
+	if err != nil {
+		// PANIK
+		panic(err)
+	}
+}
